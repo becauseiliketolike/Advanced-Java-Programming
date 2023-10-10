580db1f5b8ac92cbb626a87aafd2051f8427e070
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-10-10 19:43:39 +0545

    resource leak fix

--- Lab01/Q1.java
+++ Lab01/Q1.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class Q1 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter two numbers: ");
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int sum = a + b;
-		System.out.println("The sum is: " + sum);
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("Enter two numbers: ");
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int sum = a + b;
+			System.out.println("The sum is: " + sum);
+		}
 	}
 }
