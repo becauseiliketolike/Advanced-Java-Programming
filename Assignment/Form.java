1ff6cd883317a32f2a10efb42269949e7a2739d8
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-10-16 15:46:20 +0545

    Form done

--- Assignment/Form.java
+++ Assignment/Form.java
@@ -1,27 +1,25 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class Form {
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Registration Form");
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setSize(600, 600);
+        jFrame.setSize(600, 400);
+        jFrame.setLayout(new GridLayout(6, 1));
         
-        // Name
         JLabel nameJLabel = new JLabel("Name: ");
         JTextField nameTextField = new JTextField();
         nameTextField.setColumns(50);
 
-        // Country
         JLabel countryJLabel = new JLabel("Country: ");
         String[] country = {"USA", "UK", "Nepal"};
         JComboBox countryJComboBox = new JComboBox(country);
 
-        // Password
         JLabel passwordJLabel = new JLabel("Password: ");
         JPasswordField passwordJField = new JPasswordField();
         passwordJField.setColumns(50);
 
-        // Gender
         JLabel genderJLabel = new JLabel("Gender: ");
         JRadioButton maleButton = new JRadioButton("Male");
         JRadioButton femaleButton = new JRadioButton("Female");
@@ -29,7 +27,6 @@ public class Form {
         genderButtonGroup.add(maleButton);
         genderButtonGroup.add(femaleButton);
         
-        // Marital Status
         JLabel maritalStatusJLabel = new JLabel("Marital Status: ");
         JCheckBox marriedJCheckBox = new JCheckBox("Married");
         JCheckBox unmarriedJCheckBox = new JCheckBox("Unmarried");
@@ -39,10 +36,25 @@ public class Form {
         statusButtonGroup.add(unmarriedJCheckBox);
         statusButtonGroup.add(singleJCheckBox);
 
-        // Grid Layout
-        JPanel panel1 = 
-        
-        // jFrame.add();
+        JPanel panel1 = new JPanel();
+        panel1.add(nameJLabel); panel1.add(nameTextField);
+        JPanel panel2 = new JPanel();
+        panel2.add(passwordJLabel); panel2.add(passwordJField);
+        JPanel panel3 = new JPanel();
+        panel3.add(countryJLabel); panel3.add(countryJComboBox);
+        JPanel panel4 = new JPanel();
+        panel4.add(genderJLabel); panel4.add(maleButton); panel4.add(femaleButton);
+        JPanel panel5 = new JPanel();
+        panel5.add(maritalStatusJLabel); panel5.add(marriedJCheckBox); panel5.add(unmarriedJCheckBox); panel5.add(singleJCheckBox);
+        JPanel panel6 = new JPanel();
+        panel6.add(new JButton("Submit")); panel6.add(new JButton("Cancel"));
+
+        jFrame.add(panel1);
+        jFrame.add(panel2);
+        jFrame.add(panel3);
+        jFrame.add(panel4);
+        jFrame.add(panel5);
+        jFrame.add(panel6);
         jFrame.setVisible(true);
     }
 }
