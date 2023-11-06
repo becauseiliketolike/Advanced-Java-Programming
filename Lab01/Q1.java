6920d3423ede876c4ebfbf916c3d9fca1a6a1173
becauseiliketolike <131786997+becauseiliketolike@users.noreply.github.com>
2023-11-06 17:32:54 +0545

    remove try

--- Lab01/Q1.java
+++ Lab01/Q1.java
@@ -4,12 +4,11 @@ import java.util.Scanner;
 
 public class Q1 {
 	public static void main(String[] args) {
-		try (Scanner sc = new Scanner(System.in)) {
-			System.out.print("Enter two numbers: ");
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			int sum = a + b;
-			System.out.println("The sum is: " + sum);
-		}
+		Scanner sc = new Scanner(System.in)
+		System.out.print("Enter two numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int sum = a + b;
+		System.out.println("The sum is: " + sum);
 	}
 }
