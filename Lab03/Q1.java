426eb9cc0b01952370cf3b80f913fdd59f2e5b17
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-11-06 11:48:40 +0545

    fix

--- Lab03/Q1.java
+++ Lab03/Q1.java
@@ -1,5 +1,7 @@
 // Q.1 Write a program to take a line as input. Check if the words are palindrome. If yes write to file "palindrome.txt" otherwise write to file "NotPalindrome.txt".
 
+import java.util.Scanner;
+
 public class Q1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
