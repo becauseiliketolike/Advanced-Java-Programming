be5209d988b154743c44949e56a4d64b368855dd
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-11-06 11:44:40 +0545

    fix

--- Lab02/Q3.java
+++ Lab02/Q3.java
@@ -1,7 +1,6 @@
 // Q.3 Create a program to copy the content of "Mydata.txt" to "CopiedFile.txt".
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Q3 {
     public static void main(String[] args) {
