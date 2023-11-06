9ef0afaed3fa57d6420647fcbdad45db9e7e73e3
unknown <norden.tamang@student.trinity.edu.np>
2023-11-06 07:31:50 +0545

    Initialize Q1

--- Lab03/Q1.java
+++ Lab03/Q1.java
@@ -1,1 +1,9 @@
-// Write a program to take a line as input. Check if the words are palindrome. If yes write to file "palindrome.txt" otherwise write to file "NotPalindrome.txt".
+// Q.1 Write a program to take a line as input. Check if the words are palindrome. If yes write to file "palindrome.txt" otherwise write to file "NotPalindrome.txt".
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+    }
+}
