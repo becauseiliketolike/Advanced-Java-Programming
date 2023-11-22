fb62ea692b7f4dd94f78d3a44bd4739ebba1ecdc
becauseiliketolike <131786997+becauseiliketolike@users.noreply.github.com>
2023-11-22 09:38:12 +0545

    test input added

--- Lab03/Q1.java
+++ Lab03/Q1.java
@@ -1,5 +1,7 @@
 // Q.1 Write a program to take a line as input. Check if the words are palindrome. If yes write to file "palindrome.txt" otherwise write to file "NotPalindrome.txt".
 
+// Test input: wow jkl tenet hamro stats ramro radar good noon mood mom should madam 
+
 import java.io.*;
 import java.util.Scanner;
 
