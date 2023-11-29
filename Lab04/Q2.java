8592d2f9835433b6174e62b8c5b95cdf74f03f35
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-11-29 20:08:32 +0545

    print fix

--- Lab04/Q2.java
+++ Lab04/Q2.java
@@ -2,9 +2,9 @@
 
 class Resource {
     public int x = 0;
-    public synchronized void Increment() { 
+    public synchronized void Increment(String threadName) { 
         x++; 
-        System.out.println(x);
+        System.out.println("Running " + threadName +  ": " + x);
     }
 }
 
@@ -12,10 +12,10 @@ public class Q2 {
     public static void main(String[] args) {
         Resource resource = new Resource();
         Thread A = new Thread() {
-            public void run() { resource.Increment(); }
+            public void run() { resource.Increment("A"); }
         };
         Thread B = new Thread() {
-            public void run() { resource.Increment(); }
+            public void run() { resource.Increment("B"); }
         };
         A.start();
         B.start();
