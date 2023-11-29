baf162db7f8ffbade40a192123f4d05fc84f1299
unknown <norden.tamang@student.trinity.edu.np>
2023-11-29 07:18:57 +0545

    complete

--- Lab04/Q2.java
+++ Lab04/Q2.java
@@ -1,6 +1,5 @@
 // Q.2 Write a program in which two threads A, B will share same resource from the class Resource. Make sure that the resource are shared without exception.
 
-
 class Resource {
     public int x = 0;
     public synchronized void Increment() { 
@@ -9,41 +8,16 @@ class Resource {
     }
 }
 
-
-class A extends Thread {
-    public Resource resource;
-    public A(Resource resource) {
-        this.resource = resource;
-    }
-    public void run() {
-        try {
-            this.resource.Increment();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-}
-
-class B extends Thread {
-    public Resource resource;
-    public B(Resource resource) {
-        this.resource = resource;
-    }
-    public void run() {
-        try {
-            this.resource.Increment();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-}
-
 public class Q2 {
     public static void main(String[] args) {
         Resource resource = new Resource();
-        A a = new A(resource);
-        B b = new B(resource);
-        a.start();
-        b.start();
+        Thread A = new Thread() {
+            public void run() { resource.Increment(); }
+        };
+        Thread B = new Thread() {
+            public void run() { resource.Increment(); }
+        };
+        A.start();
+        B.start();
     }
 }
