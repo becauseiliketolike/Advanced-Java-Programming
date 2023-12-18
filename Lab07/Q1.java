7af607c5a9479bf6845a7b09d75cf1ede0968fd1
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-12-18 22:18:05 +0545

    lab 07 basics

--- Lab07/Q1.java
+++ Lab07/Q1.java
@@ -10,73 +10,70 @@
 // When clear button is clicked, clear all the user input and user selected values.
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-class SignUpForm {
-    JFrame jf = new JFrame();
+class SignUpForm implements ActionListener {
+    JFrame jf;
     JLabel jlName, jlAddress, jlCountry, jlEmail, jlMarital, jlGender, jlTerms;
+    JTextField jtfName, jtfAddress, jtfEmail, jtfMarital, jtfGender, jtfTerms;
+    JComboBox jcbCountry;
+    JCheckBox jcbMarried, jcbUnmarried, jcbSingle, jcbAccept;
+    ButtonGroup bgStatus, bgGender;
+    JRadioButton jrbMale, jrbFemale;
 
     public SignUpForm() {
-        jf = new JFrame();
+        jf = new JFrame("Sign Up Form");
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setSize(400, 400);
-        jlName = new JLabel("Name: ");
-        JTextField nameTextField = new JTextField(20);
-        jlAddress = new JLabel("Address: ");
-        JTextField addTextField = new JTextField(20);
-        jlEmail = new JLabel("Email: ");
-        JTextField emailTextField = new JTextField(20);
-        jlCountry = new JLabel("Country: ");
-        String[] country = {"USA", "UK", "Nepal"};
-        JComboBox countryJComboBox = new JComboBox(country);
-        JLabel maritalStatusJLabel = new JLabel("Marital Status: ");
-        JCheckBox marriedJCheckBox = new JCheckBox("Married");
-        JCheckBox unmarriedJCheckBox = new JCheckBox("Unmarried");
-        JCheckBox singleJCheckBox = new JCheckBox("Single");
-        ButtonGroup statusButtonGroup = new ButtonGroup();
-        statusButtonGroup.add(marriedJCheckBox);
-        statusButtonGroup.add(unmarriedJCheckBox);
-        statusButtonGroup.add(singleJCheckBox);
-        JLabel genderJLabel = new JLabel("Gender: ");
-        JRadioButton maleButton = new JRadioButton("Male");
-        JRadioButton femaleButton = new JRadioButton("Female");
-        ButtonGroup genderButtonGroup = new ButtonGroup();
-        genderButtonGroup.add(maleButton);
-        genderButtonGroup.add(femaleButton);
-        JLabel TermsJLabel = new JLabel("Terms and Policy: ");
-        JCheckBox acceptJCheckBox = new JCheckBox();
-        JPanel panel1 = new JPanel();
-        panel1.add(nameJLabel); panel1.add(nameTextField);
-        JPanel panel2 = new JPanel();
-        panel2.add(addJLabel); panel2.add(addTextField);
-        JPanel panel3 = new JPanel();
-        panel3.add(emailJLabel); panel3.add(emailTextField);
-        JPanel panel4 = new JPanel();
-        panel4.add(countryJLabel); panel4.add(countryJComboBox);
-        JPanel panel5 = new JPanel();
-        panel5.add(maritalStatusJLabel); panel5.add(marriedJCheckBox); panel5.add(unmarriedJCheckBox); panel5.add(singleJCheckBox);
-        JPanel panel6 = new JPanel();
-        panel6.add(genderJLabel); panel6.add(maleButton); panel6.add(femaleButton);
-        JPanel panel7 = new JPanel();
-        panel7.add(TermsJLabel); panel7.add(acceptJCheckBox);
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(7, 1));
-        panel.add(panel1);
-        panel.add(panel2);
-        panel.add(panel3);
-        panel.add(panel4);
-        panel.add(panel5);
-        panel.add(panel6);
-        panel.add(panel7);
+        
         jf.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0; gbc.gridy = 0;
-        jf.add(new JLabel("SIGN UP"), gbc);
-        gbc.gridy = 1;
-        jf.add(panel, gbc);
-        gbc.gridy = 2;
-        jf.add(jbSignUp, gbc);
+        
+        jlName = new JLabel("Name: ");
+        jtfName = new JTextField(20);
+        jtfName.addActionListener(this);
+        
+        jlAddress = new JLabel("Address: ");
+        jtfAddress = new JTextField(20);
+        jtfAddress.addActionListener(this);
+        
+        jlEmail = new JLabel("Email: ");
+        jtfEmail = new JTextField(20);
+        jlCountry = new JLabel("Country: ");
+        String[] country = {"USA", "UK", "Nepal"};
+        jcbCountry = new JComboBox(country);
+        jcbCountry.addActionListener(this);
+        
+        jlMarital = new JLabel("Marital Status: ");
+        jcbMarried = new JCheckBox("Married");
+        jcbMarried.addActionListener(this);
+        jcbUnmarried = new JCheckBox("Unmarried");
+        jcbUnmarried.addActionListener(this);
+        jcbSingle = new JCheckBox("Single");
+        jcbSingle.addActionListener(this);
+        bgStatus = new ButtonGroup();
+        bgStatus.add(jcbMarried);
+        bgStatus.add(jcbUnmarried);
+        bgStatus.add(jcbSingle);
+
+        jlGender = new JLabel("Gender: ");      
+        jrbMale = new JRadioButton("Male");
+        jrbMale.addActionListener(this);
+        jrbFemale = new JRadioButton("Female");
+        jrbFemale.addActionListener(this);
+        bgGender = new ButtonGroup();
+        bgGender.add(jrbMale);
+        bgGender.add(jrbFemale);
+
+        jlTerms = new JLabel("Terms and Policy: ");
+        jcbAccept = new JCheckBox();
+
         jf.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        //
     }
 }
 
