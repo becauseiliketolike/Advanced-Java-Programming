6624ef451242c2a7ebce6f24b145c260c92ae28c
becauseiliketolike <131786997+becauseiliketolike@users.noreply.github.com>
2024-01-01 07:15:01 +0545

    questions 09

---
+++ Lab09/Q.java
@@ -0,0 +1,7 @@
+// Q.1 WAP to perform CRUD operation on table: student using RowSet
+// i. JdbcRowSet
+// ii. CachedRowSet
+
+// Q.2 WAP to read the data from student table using:
+// i. ScrollableResultSet
+// ii. UpdatableResultSet
